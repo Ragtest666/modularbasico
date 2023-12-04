74c80edfc31f9eb359a69b2269ea2b213ead719a
Taquito119 <partida.andres1213@gmail.com>
2023-12-04 03:34:06 -0600

    cambio15

--- catalogo-pedidos.php
+++ catalogo-pedidos.php
@@ -276,7 +276,10 @@ $usuario = $_SESSION["nombre_usuario"];
 
                                 
 
-                            
+                                 <div class=" col-lg-4">
+                                   <h6> Costo Total:</h6>
+                                    <div ><label class="display-4" style="color: white;">$</label></div>
+                                </div>
 
                             <div class="pt-4">
                                 <div class="row">
